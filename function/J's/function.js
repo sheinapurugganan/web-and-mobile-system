8e9a39f533494a5beb4e982633c09c4da660f99b
sheinapurugganan <sheinapurugganan@gmail.com>
2026-06-21 21:46:48 +0800

    Add files via upload

---
+++ function/J's/function.js
@@ -0,0 +1,12 @@
+function normal () {
+     withParameter("Ethel Joy", "Blase");
+
+}
+function withParameter (fname, lname) {
+     alert("Hello "+ fname+" "+lname);
+
+}
+function getSum(num1,num2) {
+    const sum= num1 + num2;
+    return sum;
+}
